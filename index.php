d2425b5e44006207e19d6989fb17d1327ae7a632
KyryloVoznyi <kyrylo.voznyi@gmail.com>
2022-02-12 10:40:49 +0200

    Da

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div style="margin:50 auto;width:500px">
+        <form action="">
+            <p>Логин: <input type="text"></p>
+            <p>Пароль: <input type="password"></p>
+        </form>
+    </div>
+</body>
+
+</html>
